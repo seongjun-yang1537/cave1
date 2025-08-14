70677a3fa723f2dd331c7e09d5bd1ef19d00acce
seongjun.yang <seongjun.yang1537@5minlab.com>
2025-08-15 01:17:18 +0900

    save

---
+++ UI/Runtime/2D/Outgame/ShopPopupUI/Inventory/UIShopBagSlotElement.cs
@@ -0,0 +1,16 @@
+using Codice.CM.Common;
+using Corelib.Utils;
+using Ingame;
+using TMPro;
+using TriInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIShopBagSlotElement : UIBagSlotElement
+    {
+
+    }
+}
